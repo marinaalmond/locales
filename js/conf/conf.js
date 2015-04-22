05dcdd893ae8c86bf6bbb1677591d20df86f74f7
Marina Ilic <marina_87@yahoo.com>
2015-04-22 11:22:32 +0200

    fixes

--- js/conf/conf.js
+++ js/conf/conf.js
@@ -1,22 +1,13 @@
-<script>
-	var projmodule = angular.module('app',['ngRoute']);
-		
-	projmodle.config(['$routeProvider',
-		function($routeProvider) {
-			$routeProvider
-				.when('/route1',
-				{
-					controller: 'FirstController',
-					templateUrl: 'View1.html'
-				})
-				.when('/route2',
-				{
-					controller: 'FirstController',
-					templateUrl: 'View2.html'
-	            }).
-                otherwise({
-                    redirectTo: '/'
-                });
-			});	
-				
-</script>
+
+var app = angular.module('app', ['ngRoute']);
+	
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'FirstController',
+			templateUrl: 'view/route1.html'
+		})
+	    .otherwise({
+	        redirectTo: '/'
+	    });
+});
--- js/controllers/FirstController.js
+++ js/controllers/FirstController.js
@@ -1,18 +1,4 @@
-<script>
-		
-	var app = angular.module('app',[])
-		app.controller('FirstController',function($scope) {
-			$scope.projects= [
-				{ name: 'Project 1' },
-				{ name: 'Project 2' },
-				{ name: 'Project 3' }
-			];
-			$scope.addProject = function() {
-				$scope.projects.push({ 
-					name: $scope.newProject.name
-			<!-- kako da input gde se unosi novi projekat da nakon unosa ponovo bude prazan? -->		
-				});
-			};		
-		});				
-</script>
-	
+app.controller('FirstController', function($scope){
+
+	$scope.testme = 'Cao svete';
+});
