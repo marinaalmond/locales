43a80d19164a07219c3ad27e0ef0ce2b46d7ee00
Marina Ilic <marina_87@yahoo.com>
2015-06-03 15:03:41 +0200

    nove izmene

--- js/controllers/FirstController.js
+++ js/controllers/FirstController.js
@@ -1,8 +1,8 @@
 app.controller('FirstController', function($scope){
 
-	$scope.projects= [
+	$scope.projects = [
 		{ name: 'Project 1' },
-		{ name: 'Project 2' },
+		{ name: 'Project 2'	},
 		{ name: 'Project 3' }
 	];
 
@@ -13,7 +13,7 @@ app.controller('FirstController', function($scope){
 	    $scope.newProject.name = ''
     };
 
-	$scope.items= [
+	$scope.items = [
 		{ name: 'Languages' },
 		{ name: 'Translators' },
 		{ name:'Settings' }
@@ -24,4 +24,23 @@ app.controller('FirstController', function($scope){
 		});
 		$scope.newItem.name = ''
 	};
+
+	$scope.products = [
+		{
+		title: 'Product 1',
+		date: new Date('2015', '06', '03'), 
+		price: 55
+		},
+		{
+		title: 'Product 2',
+		date: new Date('2015', '03', '01'), 
+		price: 44
+		},
+		{
+		title: 'Product 3',
+		date: new Date('2015', '03', '23'), 
+		price: 33
+		}
+	];
+	
 });
