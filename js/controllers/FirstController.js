9f25c85885cbf86b52cd7331fd03bb26b44a5449
Marina Ilic <marina_87@yahoo.com>
2015-06-04 14:17:17 +0200

    dodala malo css za menu

--- js/controllers/FirstController.js
+++ js/controllers/FirstController.js
@@ -29,18 +29,32 @@ app.controller('FirstController', function($scope){
 		{
 		title: 'Product 1',
 		date: new Date('2015', '06', '03'), 
-		price: 55
+		price: 55,
+		likes: 0,
+		dislikes: 0
 		},
 		{
 		title: 'Product 2',
 		date: new Date('2015', '03', '01'), 
-		price: 44
+		price: 44,
+		likes: 0,
+		dislikes: 0
 		},
 		{
 		title: 'Product 3',
 		date: new Date('2015', '03', '23'), 
-		price: 33
+		price: 33,
+		likes: 0,
+		dislikes: 0
 		}
 	];
+
+	$scope.plusOne = function(index) { 
+ 		$scope.products[index].likes += 1;
+  	}
+
+  	$scope.minusOne = function(index) { 
+  		$scope.products[index].dislikes += 1;
+	}
 	
 });
